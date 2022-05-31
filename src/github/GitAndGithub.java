0c91979af025736a47ba31309e3050118e5dd9d9
jenellecardona <jnel.cardona@gmail.com>
2022-05-31 19:58:17 -0400

    2nd commit

--- src/github/GitAndGithub.java
+++ src/github/GitAndGithub.java
@@ -1,5 +1,11 @@
 package github;
 
 public class GitAndGithub {
+	public static void main(String[] args) {
+		
+		System.out.println("today we learn github");
+		System.out.println("today is a very good day");
+		
+	}
 
 }
